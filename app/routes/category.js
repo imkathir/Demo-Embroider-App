450f9540e6f896b4355b29fb42fecbb477a5e77e
Kathir <kathir.v@zohocorp.com>
2019-07-25 19:35:52 +0530

    Adds favicon

--- app/routes/category.js
+++ app/routes/category.js
@@ -2,10 +2,14 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
   model () {
-    return [
+    let arr = [
       { name:'Animals', route: 'animals' },
       { name:'Vegetables', route: 'vegetables' },
-      { name:'Birds', route: 'birds' }
+      { name:'Birds', route: 'birds' },
+      { name:'Tools' }
     ];
+    // eslint-disable-next-line no-console
+    console.log(arr.filterBy('route'));
+    return arr.filterBy('route');
   }
 });

9b2decc83fa91a518c31eed6a3e79032bbf829f5
Kathir <kathir.v@zohocorp.com>
2019-08-06 02:00:27 +0530

    LinkTo Angle bracket changes

--- config/environment.js
+++ config/environment.js
@@ -5,7 +5,7 @@ module.exports = function(environment) {
     modulePrefix: 'farmers-market',
     environment,
     rootURL: '/',
-    locationType: 'hash',
+    locationType: 'auto',
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build

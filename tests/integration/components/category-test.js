105586172812bef53e6edf696fa82b68461020a3
Kathir <kathir.v@zohocorp.com>
2019-12-12 23:41:34 +0530

    Remove test cases

--- tests/integration/components/category-test.js
+++ tests/integration/components/category-test.js
@@ -13,14 +13,5 @@ module('Integration | Component | category', function(hooks) {
     await render(hbs`{{category}}`);
 
     assert.equal(this.element.textContent.trim(), '');
-
-    // Template block usage:
-    await render(hbs`
-      {{#category}}
-        template block text
-      {{/category}}
-    `);
-
-    assert.equal(this.element.textContent.trim(), 'template block text');
   });
 });
